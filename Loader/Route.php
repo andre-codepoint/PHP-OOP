de2da491d4e2f7002c2fd0012b405686983d1e56
andre-codepoint <snsncps@gmail.com>
2024-09-28 15:27:41 +0300

    add file

--- Loader/Route.php
+++ Loader/Route.php
@@ -25,33 +25,35 @@ class Route {
         echo " Route::init ";
         $uri=$_SERVER['REQUEST_URI'];
         $segment =$uri;
-        if(strpos("?",$uri) ){
+        if(strpos("?",$uri))
             {
                 $segment=explode("?",$uri)[0];
             }
             $segment=trim($segment, "/");
 
             if(!isset($this->route[$_SERVER["REQUEST_METHOD"]])){
-                // show 404
+                echo " 404 ";
             }
             $routes=$this->route[$_SERVER["REQUEST_METHOD"]];
             for ($i=0, $count=count($routes); $i < $count; $i++) {
                 if(preg_match("#^".$routes[$i]["uri"]."$#",$segment)){
                     $params=$routes[$i]['params'];
-                    if(strpos($params,"$")!==false){
+                    if(strpos($params, "$")!==false){
                         $params=preg_replace("#^".$routes[$i]["uri"]."$#",$params,$segment);
                         $params=explode("/",$params);
-                    } else{$params=[];}
+                    } else{
+                        $params=[];
+                    }
                     $class = $routes[$i]["controller"];
                     $controller = new $class();
                     if(!method_exists($controller,$routes[$i]["action"])){
                         // show404
+                        echo " 404 ";
                     }
                     call_user_func_array([$controller,$routes[$i]["action"]],$params);
                 }
             }
-            }
-        }
+    }
 
 }
 
